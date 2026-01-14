2352d2492ab99d111538ffc5843c84233f775d3c
Aryan <aryanghugare75@gmail.com>
2026-01-15 01:32:33 +0530

    Implement thumbnail creation and management routes with multipart support

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const fastifyEnv = require("@fastify/env");
 //register plugins
 fastify.register(require("@fastify/cors"));
 fastify.register(require("@fastify/sensible"));
+fastify.register(require("@fastify/multipart"));
+fastify.register(
+  require("@fastify/static", {
+    root: path.join(__dirname, "uploads"),
+    prefix: "/uploads/", // optional: default '/'
+  })
+);
 fastify.register(require("@fastify/env"), {
   dotenv: true,
   schema: {
